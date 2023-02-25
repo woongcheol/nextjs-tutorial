43ee96222dd42befa6754a3038647112bdd45b99
Woongcheol Shin <1994_100@naver.com>
2023-02-25 19:40:00 +0900

    Migration react to next.js

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const [clicked, setClicked] = useState(0);
+
+  function handleClick() {
+    setClicked(clicked + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Click({clicked})</button>
+    </div>);
+}
